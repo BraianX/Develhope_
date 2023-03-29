1a13227fcad29e2e148148ebf8bbf25bc040ddf9
Braian <124002207+BraianX@users.noreply.github.com>
2023-03-29 18:29:10 +0200

    esercizio 1/2

--- Java_Script_esercizi/Exercise 1 - Conditional and Loops/index.js
+++ Java_Script_esercizi/Exercise 1 - Conditional and Loops/index.js
@@ -17,7 +17,7 @@ console.log(ceoSalary);
 console.log(managerSalary);
 console.log(ctoSalary);
 console.log(developerSalary);
-console.log(otherSalary);*/
+console.log(otherSalary);
 
 function calculateSalary(role) {
   let result = 0;
@@ -52,4 +52,38 @@ console.log(ceoSalary);
 console.log(managerSalary);
 console.log(ctoSalary);
 console.log(developerSalary);
-console.log(otherSalary);
+console.log(otherSalary);*/
+
+
+
+
+
+
+
+
+//esercizio 2
+
+function nicknameMap(persone) {
+  let nicknames = [];
+  for (let i = 0; i < persone.length; i++) {
+    nicknames.push(persone[i].name + '-' + persone[i].age);
+  }
+  return nicknames;
+}
+
+const persone = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const nicknames = nicknameMap(persone);
+console.log(persone);
+console.log(nicknames);
