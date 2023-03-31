6d850d5cfd6c82d96d9e667cb809c11d84db0121
Braian <124002207+BraianX@users.noreply.github.com>
2023-03-31 12:23:42 +0200

    Update index.js

--- Java_Script_esercizi/Exercise 23 - Destructuring/index.js
+++ Java_Script_esercizi/Exercise 23 - Destructuring/index.js
@@ -11,15 +11,15 @@ const person = {
 
 console.log(isAdult(person)); */
 
-function isAdult(person) {
-  return person.age >= 18;
-}
-
-const person = {
-  id: 1,
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
-};
-
-console.log(isAdult(person));
+function isAdult({age}) {
+    return age >= 18;
+  }
+  
+  const person = {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  };
+  
+  console.log(isAdult(person));
